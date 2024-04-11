76ea16aace33632846b37423f390ca98ab0680c8
Beterraba <ls6182315@gmail.com>
2024-04-11 17:08:15 -0300

    adiciona filtro para os dados do arquivo jobs.ts

--- app/jobs/route.ts
+++ app/jobs/route.ts
@@ -1,6 +1,14 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { jobs } from "@/jobs";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const url = new URL(request.url!)
+
+  const level = url.searchParams.get('level')
+  
+  if(url.searchParams.has("level")) {
+    return NextResponse.json(jobs.filter((e) => e.level === level))
+  }
+
   return NextResponse.json(jobs);
 }
